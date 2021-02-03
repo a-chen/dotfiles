53955e67a3e80c39f628fa5f8801fe317e416cac
Andrew Chen <achen.this@gmail.com>
2021-02-02 19:06:15 -0500

    changed script to move only if missing

--- create-sym-links.sh
+++ create-sym-links.sh
@@ -3,5 +3,7 @@
 # create symlinks to dot files in this repository
 ######################
 
+# move only if file exists
+[ ! -f ~/.vimrc ] || mv -v ~/.vimrc ~/.vimrc.bak
 ln -s ~/code/dotfiles/.vimrc ~/.vimrc
 
