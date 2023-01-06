58ac3eef260489064666c64ec91380ebcbc2667f
Andrew Chen <achen.this@gmail.com>
2023-01-05 22:55:06 -0500

    Switched to yadm, added README

---
+++ README.md
@@ -0,0 +1,4 @@
+Dotfiles managed by [yadm](https://yadm.io/docs/overview#)
+1. [Install](https://yadm.io/docs/install#)
+2. Get repo `yadm clone https://github.com/a-chen/dotfiles`
+3. Use `yadm <git command>` to manage files
--- create-sym-links.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#######################
-# create symlinks to dot files in this repository
-######################
-
-# move only if file exists
-[ ! -f ~/.vimrc ] || mv -v ~/.vimrc ~/.vimrc.bak
-ln -s ~/code/dotfiles/.vimrc ~/.vimrc
-
